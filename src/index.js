e5bc2bd5212a84df5c26807d8af0585c6c0ad4af
Vadym <vadikdeputat@gmail.com>
2021-07-14 22:22:16 +0200

    change preloader

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Chat from './Chat';
-import { Lines } from 'react-preloaders';
+import { Dots } from 'react-preloaders';
 import store from "./store/store";
 import { Provider } from "react-redux";
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
-            <Lines />
+            <Dots />
             <Chat />
         </Provider>
     </React.StrictMode>,
